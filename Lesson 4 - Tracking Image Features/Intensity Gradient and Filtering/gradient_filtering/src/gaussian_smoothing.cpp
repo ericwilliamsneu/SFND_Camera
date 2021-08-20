3a2576b2c0dc20dc8d73ec4869652c2257b5582a
Eric Williams <eric.williams@emerson.com>
2021-08-20 06:51:35 -0500

    Exercise 3

--- Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
+++ Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
@@ -11,7 +11,18 @@ void gaussianSmoothing1()
     // load image from file
     cv::Mat img;
     img = cv::imread("../images/img1gray.png");
+    if (img.empty())
+    {
+        printf(" Error opening image\n");
+        exit(EXIT_FAILURE);
+    }
 
+    // show result
+    string windowName = "Input Image";
+    cv::namedWindow(windowName, 1); // create window
+    cv::imshow(windowName, img);
+    cv::waitKey(0); // wait for keyboard input before continuing
+    
     // create filter kernel
     float gauss_data[25] = {1, 4, 7, 4, 1,
                             4, 16, 26, 16, 4,
@@ -19,19 +30,49 @@ void gaussianSmoothing1()
                             4, 16, 26, 16, 4,
                             1, 4, 7, 4, 1};
     cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
+    cv::Scalar kernelsum = cv::sum(kernel);
+    double kernelfactor = kernelsum[0];
+    kernel /= kernelfactor;
 
     // apply filter
     cv::Mat result;
     cv::filter2D(img, result, -1, kernel, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
 
     // show result
-    string windowName = "Gaussian Blurring";
+    windowName = "Manual Gaussian Blurring";
     cv::namedWindow(windowName, 1); // create window
     cv::imshow(windowName, result);
     cv::waitKey(0); // wait for keyboard input before continuing
 }
 
+void gaussianSmoothing()
+{
+    // load image from file
+     // load image from file
+    cv::Mat img;
+    img = cv::imread("../images/img1gray.png");
+    if (img.empty())
+    {
+        printf(" Error opening image\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // show result
+    string windowName = "Input Image";
+    cv::namedWindow(windowName, 1); // create window
+    cv::imshow(windowName, img);
+    cv::waitKey(0); // wait for keyboard input before continuing
+
+    cv::Mat dst;
+    cv::GaussianBlur(img,dst,cv::Size(3,3),0,0);
+    windowName = "Output Image";
+    cv::namedWindow(windowName, 1);
+    cv::imshow(windowName, dst);
+    cv::waitKey(0);
+}
+
 int main()
 {
     gaussianSmoothing1();
+    gaussianSmoothing();
 }
